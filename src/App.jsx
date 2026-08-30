8dfd3117632276b4d9eeda9a88f1372ad7038b94
saray <sarayneang271@gmail.com>
2026-08-30 16:29:36 +0700

    Render_all_page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Destination from './pages/admin/destinations/Destination'
+import { RegisterForm } from './pages/auth/RegisterForm'
+import LoginPage from './pages/auth/LoginPage'
+import BookingList from './components/booking/BookingList'
+import Customers from './components/admin/customers/Customers'
+import Destinations from './pages/admin/destinations/destinations'
+import RevenueOverview from './components/tour/RevenueOverview'
+import Sidebar from './components/layout/sidebar'
+import Header from './components/layout/header'
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Destination />} />
+          
+          <Route path='/register' element={<RegisterForm/>}/>
+          <Route path='/login' element={<LoginPage/>}/>
+          <Route path='/listbooking' element={<BookingList/>}/>
+          <Route path='/customer' element={<Customers/>}/>
+          <Route path='/destination' element={<Destination/>}/>
+          <Route path='/table_destinations' element={<Destinations/>}/>
+          <Route path='/overview' element={<RevenueOverview/>}/>
+          <Route path='/sidebar' element={<Sidebar/>}/>
+          <Route path='/header' element={<Header/>}/>
+         
         </Routes>
       </BrowserRouter>
     </>
