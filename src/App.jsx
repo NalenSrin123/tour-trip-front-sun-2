f1b27a1da298d1d3cfe3d0200fa11a8972b9e2d2
El Roza <rozael168@gmail.com>
2026-09-06 14:54:22 +0700

    combine sidebar and header in one file

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Destination from "./pages/admin/destinations/Destination";
 import GuildList from "./pages/admin/guides/GuidesList";
+import Header from "./components/layout/header";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<GuildList />} />
+          <Route path="/" element={<Header />} />
         </Routes>
       </BrowserRouter>
     </>
