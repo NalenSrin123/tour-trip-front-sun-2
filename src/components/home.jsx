f38356885684563cb4ac447d0cb240bd00231814
Soheng Horn <sohenghorn71-ctrl@gmail.com>
2026-09-06 12:50:56 +0700

    Add header footer components

---
+++ src/components/home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Welcome to TripGo</h1>
+      <p>This is the homepage.</p>
+    </div>
+  );
+};
+
+export default Home;
