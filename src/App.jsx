04800f8c41a3ea5c2981b58480bf733c6dc66e32
Oudom Neang <oudomneang01@gmail.com>
2026-09-10 20:09:33 +0700

    create feature packet

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,9 @@ import GuidesList from "./pages/admin/guides/GuidesList";
 //import Destination from './pages/admin/destinations/Destination'
 import CreateCustomer from './pages/admin/customers/create_customer'
 import CreateUsers from './pages/admin/users/CreateUsers'
+import FeaturedTourPackages from './pages/public/Tour_packages'
+
+
 function App() {
   return (
     <>
@@ -28,10 +31,11 @@ function App() {
           <Route path='/table_destinations' element={<Destinations/>}/>
           <Route path='/overview' element={<RevenueOverview/>}/>
           <Route path='/sidebar' element={<Sidebar/>}/>
-         
+          <Route path='/createcustomer' element={<CreateCustomer/>}/>
           <Route path='/create-user' element={<CreateUsers />} />
           <Route path='/users' element={<PageListUser />} />
           <Route path="/guides" element={<GuidesList />} />
+          <Route path="/tourpackages" element={<FeaturedTourPackages />} />
       </Routes>
     
     </>
